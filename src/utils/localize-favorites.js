141efaf6ff04e0d0ce716afe645daf63b3273734
Tottis1986 <pkonidis86@gmail.com>
2020-10-27 03:30:11 +0200

    7: Refactor Favorites Local Storage to use Reducer
    - Due to the many different operations I decided to change the logic a
    little bit so that the favorites lists use a reducer that accept a
    certain group of actions
    - ACTIONS are defined and exposed as a constant in the
    `localize-favorites.js` file

--- src/utils/localize-favorites.js
+++ src/utils/localize-favorites.js
@@ -1,19 +1,52 @@
 import React from "react";
 
-export function useFavoritesState(key) {
-    const [favourites, setFavourites] = React.useState(() => {
-      const stickyValue = window.localStorage.getItem(key);
-      return stickyValue !== null
-        ? JSON.parse(stickyValue)
-        : {};
-    });
+export const ACTIONS = {
+  REMOVE_FAVORITE_LAUNCH: 'rm-fav-launch',
+  ADD_FAVORITE_LAUNCH: 'add-fav-launch',
+  REMOVE_FAVORITE_PAD: 'rm-fav-pad',
+  ADD_FAVORITE_PAD: 'add-fav-pad',
+  CLEAR_FAVORITE_LAUNCHES: 'clear-launches',
+  CLEAR_FAVORITE_PADS: 'clear-launches'
+}
 
-    React.useEffect(() => {
+function favoritesReducer(favorites, action) {
+
+  switch (action.type) {
+    case ACTIONS.REMOVE_FAVORITE_LAUNCH: {
+      delete favorites[action.payload.flight_number.toString()]
+      
+      console.log("favorites", favorites)
+      return favorites
+    }
+    case ACTIONS.ADD_FAVORITE_LAUNCH: {
+      favorites[action.payload.flight_number.toString()] = action.payload
+      console.log("favorites", favorites)
+      return favorites
+    }
+    case ACTIONS.REMOVE_FAVORITE_PAD: {
+      delete favorites[action.payload.site_id.toString()]
+      console.log("favorites", favorites)
+      return favorites
+    }
+    case ACTIONS.ADD_FAVORITE_PAD: {
+      favorites[action.payload.site_id.toString()] = action.payload
+      console.log("favorites", favorites)
+      return favorites
+    }
+    case ACTIONS.CLEAR_FAVORITE_LAUNCHES:
+      return {}
+    case ACTIONS.CLEAR_FAVORITE_PADS:
+      return {}
+    default:
+      return favorites
+  }
+}
+
+export function useFavoritesState(key) {
+    const [favourites, setFavourites] = React.useReducer( favoritesReducer, {},  () => {
       const data = localStorage.getItem(key)
-      if (data) {
-        setFavourites(JSON.parse(data))
-      }
-    }, [key])
+      return data ? JSON.parse(data) : {}
+    });
 
     React.useEffect(() => {
       window.localStorage.setItem(key, JSON.stringify(favourites));
